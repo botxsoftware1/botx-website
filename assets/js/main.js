842806061e37183c4beac9895e61a9a2311c7551
Varunbotx <87224305+Varunbotx@users.noreply.github.com>
2021-08-26 00:17:24 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,20 @@
 $(document).ready(function () {
-    var typed = new Typed(".typing", {
-        strings: ["to be the change","for you to be the change","to be the future"],
-        typeSpeed: 100,
-        backSpeed: 30,
-        loop: true
-    });
 
-    $('.dismiss, .overlay').on('click', function() {
+    $('.dismiss, .overlay').on('click', function () {
         $('.sidebar').addClass('notActive');
         $('.sidebar').removeClass('active');
     });
 
-    $('.menu_text,label').on('click', function(e) {
+    $('.menu_text, .vector label').on('click', function (e) {
         e.preventDefault();
         $('.sidebar').addClass('active');
         $('.sidebar').removeClass('notActive');
+    });
+
+    var typed = new Typed(".typing", {
+        strings: ["to be the change", "for you to be the change", "to be the future"],
+        typeSpeed: 100,
+        backSpeed: 30,
+        loop: true
     });
 });
